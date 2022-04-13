a98b80c40319113df1f499a22d32c32569bb555c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-14 00:47:41 +0200

    Completely delete root command's RunE function

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,8 @@
 package cmd
 
 import (
-	"context"
-	"fmt"
 	"os"
 
-	"github.com/google/go-github/v43/github"
 	"github.com/spf13/cobra"
 )
 
@@ -14,38 +11,10 @@ var rootCmd = &cobra.Command{
 	Use:   "ghdump <username>",
 	Short: "Bulk clone GitHub repos for backup",
 	Long: `ghdump allows you to automatically clone your (or someone else's)
-repositories.
+repositories from GitHub.
 
-Issuing ghdump without any subcommand will show some info
-about a user.`,
-	Args: cobra.ExactArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		client := github.NewClient(nil)
-		username := args[0]
-
-		fmt.Printf("User %s\n", username)
-		
-		const MAX_REPOS = 100
-
-		opt := &github.RepositoryListOptions{
-			ListOptions: github.ListOptions{PerPage: MAX_REPOS},
-		}
-		repos, _, err := client.Repositories.List(ctx, username, opt)
-		if err != nil {
-			return err
-		}
-		
-		count := len(repos)
-		fmt.Printf("Repo count (public): %d", count)
-		if count >= MAX_REPOS {
-			fmt.Print(" or more")
-		}
-
-		fmt.Println()
-
-		return nil
-	},
+It's useful if you want to backup your data,
+move to a different Git hosting etc.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
