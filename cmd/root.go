1b97eb4081faf8a131f74514b4c02d1241406af5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-13 23:55:33 +0200

    Create the root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,51 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
 	"os"
 
+	"github.com/google/go-github/v43/github"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "ghdump",
+	Use:   "ghdump <username>",
 	Short: "Bulk clone GitHub repos for backup",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Long: `ghdump allows you to automatically clone your (or someone else's)
+repositories.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+Issuing ghdump without any subcommand will show some info
+about a user.`,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+		client := github.NewClient(nil)
+		username := args[0]
+
+		fmt.Printf("User %s\n", username)
+		
+		const MAX_REPOS = 100
+
+		opt := &github.RepositoryListOptions{
+			ListOptions: github.ListOptions{PerPage: MAX_REPOS},
+		}
+		repos, _, err := client.Repositories.List(ctx, username, opt)
+		if err != nil {
+			return err
+		}
+		
+		count := len(repos)
+		fmt.Printf("Repo count (public): %d", count)
+		if count >= MAX_REPOS {
+			fmt.Print(" or more")
+		}
+
+		fmt.Println()
+
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -39,5 +66,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
